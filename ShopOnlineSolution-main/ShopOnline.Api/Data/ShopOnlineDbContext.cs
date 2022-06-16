cb3b4bc5eb5df29d8a95a37fbf41d659b1a31aea
Your Name <you@example.com>
2022-06-16 14:29:45 +0700

    ShopOnlineSolution-main

---
+++ ShopOnlineSolution-main/ShopOnline.Api/Data/ShopOnlineDbContext.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Data
+{
+	public class ShopOnlineDbContext : DbContext
+	{
+		public ShopOnlineDbContext(DbContextOptions<ShopOnlineDbContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			//Products
+			//Beauty Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 1,
+				Name = "Balo Challenger",
+				Description = "Phan thuong game thu top 100 td",
+				ImageURL = "/photos/Balo/TD.jpg",
+				Price = 350,
+				Qty = 100,
+				CategoryId = 1
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 2,
+				Name = "Balo Fpx",
+				Description = "Balo of FPX Team",
+				ImageURL = "/photos/Balo/FPX.jpg",
+				Price = 400,
+				Qty = 45,
+				CategoryId = 1
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 3,
+				Name = "Balo IG ",
+				Description = "Balo of IvitusGaming",
+				ImageURL = "/photos/Balo/IG.jpg",
+				Price = 450,
+				Qty = 30,
+				CategoryId = 1
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 4,
+				Name = "Balo T1",
+				Description = "Balo of SKTelecomT1",
+				ImageURL = "/photos/Balo/T1.jpg",
+				Price = 350,
+				Qty = 60,
+				CategoryId = 1
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 5,
+				Name = "Balo TSM",
+				Description = "Balo of TeamSoloMid",
+				ImageURL = "/photos/Balo/TSM.jpg",
+				Qty = 400,
+				CategoryId = 1
+
+			});
+			//Electronics Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 6,
+				Name = "Jacket DW",
+				Description = "The jacket for Damwon Gaming",
+				ImageURL = "/photos/Jacket/dw.jpg",
+				Price = 700,
+				Qty = 120,
+				CategoryId = 3
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 7,
+				Name = "EDG Jacket",
+				Description = "The Jacket for EdwardGaming Team",
+				ImageURL = "/photos/Jacket/edg.jpg",
+				Price = 800,
+				Qty = 200,
+				CategoryId = 3
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 8,
+				Name = "G2 Jacket",
+				Description = "The jacket for the match of G2 team",
+				ImageURL = "/photos/Jacket/g2.jpg",
+				Price = 650,
+				Qty = 300,
+				CategoryId = 3
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 9,
+				Name = "GenG Jacket",
+				Description = "The jacket for GenG team - The Team 1st of Korea",
+				ImageURL = "/photos/Jacket/geng.jpg",
+				Price = 600,
+				Qty = 20,
+				CategoryId = 3
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 10,
+				Name = "Fnc Jacket",
+				Description = "It the Best",
+				ImageURL = "/photos/Jacket/fnc.jpg",
+				Price = 800,
+				Qty = 15,
+				CategoryId = 3
+
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 11,
+				Name = "Ahri suit",
+				Description = "For Ahri Cosplayer",
+				ImageURL = "/photos/suit/ahri.jpg",
+				Price = 1500,
+				Qty = 60,
+				CategoryId = 3
+			});
+			//Furniture Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 12,
+				Name = "Kda suit",
+				Description = "For Cosplayer KDA",
+				ImageURL = "/photos/suit/kda.jpg",
+				Price = 1700,
+				Qty = 212,
+				CategoryId = 2
+			});
+
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 13,
+				Name = "Leblanc Suit",
+				Description = "Cosplay suit champ Leblanc",
+				ImageURL = "/photos/suit/lb.jpg",
+				Price = 2000,
+				Qty = 112,
+				CategoryId = 2
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 14,
+				Name = "LuLu Suit",
+				Description = "Very beautiful suit for cosplayer",
+				ImageURL = "/photos/suit/lulu.jpg",
+				Price = 1700,
+				Qty = 90,
+				CategoryId = 2
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 15,
+				Name = "Neeko Suit",
+				Description = "make u feel like real neeko",
+				ImageURL = "/photos/suit/nekko.jpg",
+				Price = 2100,
+				Qty = 95,
+				CategoryId = 2
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 16,
+				Name = "TL T-Shirt",
+				Description = "The T-Shirt of TeamLiquid for the match",
+				ImageURL = "/photos/T-Shirt/tlts.jpg",
+				Price = 150,
+				Qty = 100,
+				CategoryId = 2
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 17,
+				Name = "C9 T-Shirt",
+				Description = "The T-Shirt with logo of Could 9 Team",
+				ImageURL = "/photos/T-Shirt/c9ts.jpg",
+				Price = 200,
+				Qty = 73,
+				CategoryId = 2
+			});
+			//Shoes Category
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 18,
+				Name = "FPX T-Shirt",
+				Description = "The T-Shirt for fans of FPX Team",
+				ImageURL = "/photos/T-Shirt/fxpts.jpg",
+				Price = 250,
+				Qty = 50,
+				CategoryId = 4
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 19,
+				Name = "G2 Team T-shirt",
+				Description = "T-Shirt Gamer of G2 Team using for match",
+				ImageURL = "/photos/T-Shirt/g2ts.jpg",
+				Price = 150,
+				Qty = 60,
+				CategoryId = 4
+			});
+			modelBuilder.Entity<Product>().HasData(new Product
+			{
+				Id = 20,
+				Name = "GenG T-Shirt",
+				Description = "GenG is the best team in Korea",
+				ImageURL = "/photos/T-Shirt/gets.jpg",
+				Price = 200,
+				Qty = 70,
+				CategoryId = 4
+			});
+
+			//Add users
+			modelBuilder.Entity<User>().HasData(new User
+			{
+				Id = 1,
+				UserName = "Tuong"
+
+			});
+			modelBuilder.Entity<User>().HasData(new User
+			{
+				Id = 2,
+				UserName = "Tinh"
+
+			});
+
+			//Create Shopping Cart for Users
+			modelBuilder.Entity<Cart>().HasData(new Cart
+			{
+				Id = 1,
+				UserId = 1
+
+			});
+			modelBuilder.Entity<Cart>().HasData(new Cart
+			{
+				Id = 2,
+				UserId = 2
+
+			});
+			//Add Product Categories
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 1,
+				Name = "Balo",
+				IconCSS = "fas fa-spa"
+			});
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 2,
+				Name = "Jacket",
+				IconCSS = "fas fa-couch"
+			});
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 3,
+				Name = "suit",
+				IconCSS = "fas fa-headphones"
+			});
+			modelBuilder.Entity<ProductCategory>().HasData(new ProductCategory
+			{
+				Id = 4,
+				Name = "T-shirt",
+				IconCSS = "fas fa-shoe-prints"
+			});
+
+
+		}
+
+		public DbSet<Cart> Carts { get; set; }
+		public DbSet<CartItem> CartItems { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<User> Users { get; set; }
+
+	}
+}
---
+++ ShopOnlineSolution-main/ShopOnline.Api/Migrations/20220616060521_initial.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopOnline.Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IconCSS = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "ProductCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Carts",
+                columns: new[] { "Id", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "IconCSS", "Name" },
+                values: new object[,]
+                {
+                    { 1, "fas fa-spa", "Balo" },
+                    { 2, "fas fa-couch", "Jacket" },
+                    { 3, "fas fa-headphones", "suit" },
+                    { 4, "fas fa-shoe-prints", "T-shirt" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "Tuong" },
+                    { 2, "Tinh" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Qty" },
+                values: new object[,]
+                {
+                    { 1, 1, "Phan thuong game thu top 100 td", "/photos/Balo/TD.jpg", "Balo Challenger", 350m, 100 },
+                    { 2, 1, "Balo of FPX Team", "/photos/Balo/FPX.jpg", "Balo Fpx", 400m, 45 },
+                    { 3, 1, "Balo of IvitusGaming", "/photos/Balo/IG.jpg", "Balo IG ", 450m, 30 },
+                    { 4, 1, "Balo of SKTelecomT1", "/photos/Balo/T1.jpg", "Balo T1", 350m, 60 },
+                    { 5, 1, "Balo of TeamSoloMid", "/photos/Balo/TSM.jpg", "Balo TSM", 0m, 400 },
+                    { 6, 3, "The jacket for Damwon Gaming", "/photos/Jacket/dw.jpg", "Jacket DW", 700m, 120 },
+                    { 7, 3, "The Jacket for EdwardGaming Team", "/photos/Jacket/edg.jpg", "EDG Jacket", 800m, 200 },
+                    { 8, 3, "The jacket for the match of G2 team", "/photos/Jacket/g2.jpg", "G2 Jacket", 650m, 300 },
+                    { 9, 3, "The jacket for GenG team - The Team 1st of Korea", "/photos/Jacket/geng.jpg", "GenG Jacket", 600m, 20 },
+                    { 10, 3, "It the Best", "/photos/Jacket/fnc.jpg", "Fnc Jacket", 800m, 15 },
+                    { 11, 3, "For Ahri Cosplayer", "/photos/suit/ahri.jpg", "Ahri suit", 1500m, 60 },
+                    { 12, 2, "For Cosplayer KDA", "/photos/suit/kda.jpg", "Kda suit", 1700m, 212 },
+                    { 13, 2, "Cosplay suit champ Leblanc", "/photos/suit/lb.jpg", "Leblanc Suit", 2000m, 112 },
+                    { 14, 2, "Very beautiful suit for cosplayer", "/photos/suit/lulu.jpg", "LuLu Suit", 1700m, 90 },
+                    { 15, 2, "make u feel like real neeko", "/photos/suit/nekko.jpg", "Neeko Suit", 2100m, 95 },
+                    { 16, 2, "The T-Shirt of TeamLiquid for the match", "/photos/T-Shirt/tlts.jpg", "TL T-Shirt", 150m, 100 },
+                    { 17, 2, "The T-Shirt with logo of Could 9 Team", "/photos/T-Shirt/c9ts.jpg", "C9 T-Shirt", 200m, 73 },
+                    { 18, 4, "The T-Shirt for fans of FPX Team", "/photos/T-Shirt/fxpts.jpg", "FPX T-Shirt", 250m, 50 },
+                    { 19, 4, "T-Shirt Gamer of G2 Team using for match", "/photos/T-Shirt/g2ts.jpg", "G2 Team T-shirt", 150m, 60 },
+                    { 20, 4, "GenG is the best team in Korea", "/photos/T-Shirt/gets.jpg", "GenG T-Shirt", 200m, 70 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+        }
+    }
+}
